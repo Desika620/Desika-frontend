7a936e676cce8f59b19101f5d6b6f4923ff56cd8
Desika620 <desikaazzahra@gmail.com>
2025-07-02 19:51:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/portofolio" element={<Portofolio />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/blog" element={<Blog />} />
           </Routes>
